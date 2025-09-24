1b2ab65610f28276aba21b3b2a578be163ed7133
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-24 16:48:08 +0200

    chore: add base implementation for Paper module

---
+++ surf-hologram-paper/src/main/kotlin/dev/slne/surf/hologram/paper/PaperMain.kt
@@ -0,0 +1,17 @@
+package dev.slne.surf.hologram.paper
+
+import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
+
+class PaperMain : SuspendingJavaPlugin() {
+    override fun onLoad() {
+        super.onLoad()
+    }
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
