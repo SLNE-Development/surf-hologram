d521fecca3107a980791666c592b4a2a07251565
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-25 16:51:59 +0200

    feat: enhance HologramService with creation, deletion, and editing methods in core and Paper modules

--- surf-hologram-core/src/main/kotlin/dev/slne/surf/hologram/core/service/HologramService.kt
+++ surf-hologram-core/src/main/kotlin/dev/slne/surf/hologram/core/service/HologramService.kt
@@ -1,17 +1,26 @@
 package dev.slne.surf.hologram.core.service
 
 import dev.slne.surf.hologram.api.hologram.Hologram
+import dev.slne.surf.hologram.api.hologram.HologramType
+import dev.slne.surf.hologram.api.hologram.location.HologramLocation
 import dev.slne.surf.hologram.core.registry.hologramRegistry
 import dev.slne.surf.surfapi.core.api.util.requiredService
+import net.kyori.adventure.text.Component
 
 interface HologramService {
-    fun createHologram(hologram: Hologram): Hologram?
-    fun deleteHologram(hologram: Hologram): Boolean
+    fun createHologram(
+        name: String,
+        type: HologramType,
+        centerLocation: HologramLocation,
+        displayedText: Component
+    ): Hologram
+
+    fun deleteHologram(hologram: Hologram)
     fun deleteHologram(name: String) = hologramRegistry.getHologram(name)?.let {
         deleteHologram(it)
     }
 
-    fun editHologram(hologram: Hologram, block: Hologram.() -> Unit): Hologram
+    fun editHologramSaving(hologram: Hologram, block: Hologram.() -> Unit): Hologram
 
     companion object {
         val INSTANCE = requiredService<HologramService>()
--- surf-hologram-paper/src/main/kotlin/dev/slne/surf/hologram/paper/service/HologramServiceImpl.kt
+++ surf-hologram-paper/src/main/kotlin/dev/slne/surf/hologram/paper/service/HologramServiceImpl.kt
@@ -2,23 +2,88 @@ package dev.slne.surf.hologram.paper.service
 
 import com.google.auto.service.AutoService
 import dev.slne.surf.hologram.api.hologram.Hologram
+import dev.slne.surf.hologram.api.hologram.HologramType
+import dev.slne.surf.hologram.api.hologram.location.HologramLocation
+import dev.slne.surf.hologram.api.util.forEachViewer
+import dev.slne.surf.hologram.core.registry.hologramRegistry
 import dev.slne.surf.hologram.core.service.HologramService
+import dev.slne.surf.hologram.paper.hologram.HologramMetaDataImpl
+import dev.slne.surf.hologram.paper.hologram.types.BouncingHologramImpl
+import dev.slne.surf.hologram.paper.hologram.types.SimpleHologramImpl
+import dev.slne.surf.surfapi.core.api.util.random
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.util.Services
 
 @AutoService(HologramService::class)
 class HologramServiceImpl : HologramService, Services.Fallback {
-    override fun createHologram(hologram: Hologram): Hologram? {
-        TODO("Not yet implemented")
+    override fun createHologram(
+        name: String,
+        type: HologramType,
+        centerLocation: HologramLocation,
+        displayedText: Component
+    ): Hologram {
+        hologramRegistry.getHologram(name)?.let {
+            return it
+        }
+
+        val meta = HologramMetaDataImpl(
+            name,
+            random.nextInt(),
+            random.nextInt(),
+            1f,
+            1f
+        )
+        val hologram = when (type) {
+            HologramType.FIXED, HologramType.ROTATING -> SimpleHologramImpl(
+                meta,
+                type,
+                centerLocation,
+                displayedText,
+                null
+            )
+
+            HologramType.BOUNCING -> BouncingHologramImpl(
+                meta,
+                type,
+                centerLocation,
+                displayedText,
+                null,
+                0,
+                0.25,
+                1.0 to 1,
+                true
+            )
+        }
+
+        hologramRegistry.registerHologram(hologram)
+
+        return hologram
     }
 
-    override fun deleteHologram(hologram: Hologram): Boolean {
-        TODO("Not yet implemented")
+    override fun deleteHologram(hologram: Hologram) {
+        hologram.forEachViewer {
+            it.hideHologram(hologram)
+        }
+
+        hologramRegistry.unregisterHologram(hologram)
     }
 
-    override fun editHologram(
+    override fun editHologramSaving(
         hologram: Hologram,
         block: Hologram.() -> Unit
     ): Hologram {
-        TODO("Not yet implemented")
+        hologram.forEachViewer {
+            it.hideHologram(hologram)
+        }
+        hologramRegistry.unregisterHologram(hologram)
+
+        hologram.block()
+
+        hologramRegistry.registerHologram(hologram)
+        hologram.forEachViewer {
+            it.showHologram(hologram)
+        }
+
+        return hologram
     }
 }
