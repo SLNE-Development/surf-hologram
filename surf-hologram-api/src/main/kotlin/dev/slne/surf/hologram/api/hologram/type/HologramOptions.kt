e1f532a576c46ea1d525fc9bc479d6d610fa2276
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-29 17:42:38 +0200

    refactor: add default values to `BouncingHologramOptions` and `ScoreboardHologramOptions` properties
    - Introduced default values for `bounceHeight` and `bounceSpeed` in `BouncingHologramOptions`.
    - Added default values for `placementRange` and `scoreUnit` in `ScoreboardHologramOptions` to improve usability.

--- surf-hologram-api/src/main/kotlin/dev/slne/surf/hologram/api/hologram/type/HologramOptions.kt
+++ surf-hologram-api/src/main/kotlin/dev/slne/surf/hologram/api/hologram/type/HologramOptions.kt
@@ -5,14 +5,14 @@ import dev.slne.surf.hologram.api.hologram.BounceSpeed
 interface HologramOptions
 
 data class BouncingHologramOptions(
-    var bounceHeight: Double,
-    var bounceSpeed: BounceSpeed
+    var bounceHeight: Double = 1.0,
+    var bounceSpeed: BounceSpeed = 0.1
 ) : HologramOptions
 
 class SimpleHologramOptions : HologramOptions
 
 data class ScoreboardHologramOptions(
-    var placementRange: IntRange,
-    var scoreUnit: String
+    var placementRange: IntRange = 1..10,
+    var scoreUnit: String = "P."
 ) : HologramOptions
 
